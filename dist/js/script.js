9cca075f1ec674f2f81fc9b1210e0b267767cef8
unknown <you@example.com>
2018-12-24 22:40:28 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,34 @@
-$(document).ready(function () {
-    // $('.selectpicker').selectpicker({
-    //     width: '100%'
-    // });
+$(function () {
+    //   Banner
+    $('.banner-slide').slick({
+        dots: true,
+        arrows: false
+    });
+
+    // Card menu
+    $('.card-menu .card-body').slimScroll({
+        height: '300px',
+        color: '#14ACE9',
+        size: '1rem',
+        alwaysVisible: true,
+        railVisible: true,
+        railColor: "white",
+        wheelStep: 100,
+    });
+
+    // Place images slide
+    $('.place-images-slide').slick({
+        infinite: true,
+        slidesToShow: 4,
+        autoplay: true,
+        dots: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                infinite: true,
+                arrows: false,
+            }
+        }]
+    });
 });
