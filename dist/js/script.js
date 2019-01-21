c09d61bda98d6dc1961f8d76e21f5098e8328b48
Rathanont Sukhapant <rathanont.s@gmail.com>
2019-01-21 09:31:12 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -86,4 +86,15 @@ $(function () {
             rightArrow: "<span><img src='./dist/images/arrow-right.png' alt='arrow-right'></span>"
         });
     });
+
+
+});
+
+$(document).ready(function () {
+    // Album detail
+    $('.grid').masonry({
+        itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+        columnWidth: '.grid-item',
+        percentPosition: true
+    });
 });
